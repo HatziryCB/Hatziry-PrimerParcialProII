c24ae5f35f21945a59f6a967bc221529aa044564
Hatziry Chacón Berreondo <hchaconb@miumg.edu.gt>
2022-08-13 10:29:07 -0600

    Creando descripcion de libros

--- src/java/NewServlet.java
+++ src/java/NewServlet.java
@@ -43,21 +43,84 @@ public class NewServlet extends HttpServlet {
                     request.getParameter("tapa"),
                     request.getParameter("editorial"),
                     Integer.parseInt(request.getParameter("año")));
-            
+
             if (registroLibro == null) {
                 registroLibro = new RegistroLibro();
             }
             registroLibro.almacenar(libros);
             vector = registroLibro.mostrar();
             int iterador = 0;
+
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");
+            out.println("<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct\" crossorigin=\"anonymous\"></script>"
+                    + "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css\" integrity=\"sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N\" crossorigin=\"anonymous\">\n");
+            out.println("<title>Registro Libro</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("<nav class=\"navbar navbar-expand-lg navbar navbar-dark bg-dark\">\n"
+                    + "                <a class=\"navbar-brand\" href=\"#\">\n"
+                    + "                    <img src=\"https://icon-library.com/images/home-icon-png/home-icon-png-28.jpg\" width=\"30\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">\n"
+                    + "                    Home\n"
+                    + "                </a>\n"
+                    + "                <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n"
+                    + "                    <span class=\"navbar-toggler-icon\"></span>\n"
+                    + "                </button>\n"
+                    + "\n"
+                    + "                <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\n"
+                    + "                    <ul class=\"navbar-nav mr-auto\">\n"
+                    + "                        <li class=\"nav-item active\">\n"
+                    + "                            <a class=\"nav-link\" href=\"#\">Link1 <span class=\"sr-only\">(current)</span></a>\n"
+                    + "                        </li>\n"
+                    + "                        <li class=\"nav-item active\">\n"
+                    + "                            <a class=\"nav-link\" href=\"#\">Link2</a>\n"
+                    + "                        </li>\n"
+                    + "                        <li class=\"nav-item active dropdown\">\n"
+                    + "                            <a class=\"nav-link dropdown-toggle\" href=\"#\" role=\"button\" data-toggle=\"dropdown\" aria-expanded=\"false\">\n"
+                    + "                                Categorías\n"
+                    + "                            </a>\n"
+                    + "                            <div class=\"dropdown-menu\">\n"
+                    + "                                <a class=\"dropdown-item\" href=\"#\">Action</a>\n"
+                    + "                                <a class=\"dropdown-item\" href=\"#\">Another action</a>\n"
+                    + "                                <div class=\"dropdown-divider\"></div>\n"
+                    + "                                <a class=\"dropdown-item\" href=\"#\">Something else here</a>\n"
+                    + "                            </div>\n"
+                    + "                        </li>\n"
+                    + "                        <li class=\"nav-item active\">\n"
+                    + "                            <a class=\"nav-link disabled\">Link3</a>\n"
+                    + "                        </li>\n"
+                    + "                    </ul>\n"
+                    + "                    <form class=\"form-inline my-2 my-lg-0\">\n"
+                    + "                        <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Busqueda\" aria-label=\"Search\">\n"
+                    + "                        <button class=\"btn btn-outline-success my-2 my-sm-0\" type=\"submit\">Buscar</button>\n"
+                    + "                    </form>\n"
+                    + "                </div>\n"
+                    + "            </nav>");
+            out.println("<div class=\"mx-auto mt-5\" style=\"width: 600px;\">\n");
+            out.println("<div class=\"shadow-lg p-3 mb-5 bg-white rounded\">"
+                    + "<h4 class=\"display-4 mt-5 mx-5\">Datos de Usuario</h4>"
+                    + "<dl class=\"row my-5 mx-5\">\n"
+                    + "  <dt class=\"col-sm-3\">Nombre </dt>\n"
+                    + "  <dd class=\"col-sm-9\"> " + libros.getNombre() + "</dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3\">Código</dt>\n"
+                    + "  <dd class=\"col-sm-9\">\n"
+                    + "    <p>" + libros.getCodigo() + "</p>\n"
+                    + "  </dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3\">Tipo de tapa</dt>\n"
+                    + "  <dd class=\"col-sm-9\">" + libros.getTapa() + "</dd>\n"
+                    + "\n"
+                    + "  <dt class=\"col-sm-3 text-truncate\">Editorial</dt>\n"
+                    + "  <dd class=\"col-sm-9\">" + libros.getEditorial() + "</dd>\n"
+                    + "\n"
+                    + "<dt class=\"col-sm-3\">Año</dt>\n"
+                    + "  <dd class=\"col-sm-9\">" + libros.getAño() + "</dd>\n"
+                    + "<a class=\"btn btn-primary\" href=\"index.html\" role=\"button\">Registrar otro cliente</a>"
+                    + "</div>");
+            out.println("</div>");
             out.println("</body>");
             out.println("</html>");
         }
